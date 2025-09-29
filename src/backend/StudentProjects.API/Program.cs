02914bf01cfca54e7c5b951940d8e4ce10870917
seaz96 <seaz96@proton.me>
2025-09-29 23:25:57 +0500

    init backend

---
+++ src/backend/StudentProjects.API/Program.cs
@@ -0,0 +1,30 @@
+using Serilog;
+using StudentProjects.API.Utility;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((_, lc) => lc.GetConfiguration());
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        corsPolicyBuilder =>
+        {
+            corsPolicyBuilder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowedToAllowWildcardSubdomains()
+                .AllowCredentials();
+        });
+});
+
+var app = builder.Build();
+app.MapOpenApi();
+app.UseCors("AllowAll");
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
---
+++ src/backend/StudentProjects.API/Utility/SerilogHelper.cs
@@ -0,0 +1,26 @@
+using Serilog;
+using Serilog.Events;
+
+namespace StudentProjects.API.Utility;
+
+public static class SerilogHelper
+{
+    public static void AddLoggerServices(this IServiceCollection services)
+    {
+        services.AddSingleton(Log.Logger);
+    }
+
+    public static void GetConfiguration(this LoggerConfiguration loggerConfiguration)
+    {
+        const string logFormat =
+            "[{Timestamp:yyyy.MM.dd HH:mm:ss:ms}] [{Level}] [T-{TraceId}] {Message}{NewLine}{Exception}";
+
+        loggerConfiguration
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
+            .MinimumLevel.Override("System", LogEventLevel.Information)
+            .MinimumLevel.Is(LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .WriteTo.Async(option => { option.Console(LogEventLevel.Information, logFormat); });
+    }
+}
