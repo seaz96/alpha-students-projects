6733fdc35e176f5586b9fefe8f145c885f37f2b3
seaz96 <seaz96@proton.me>
2025-11-10 23:37:52 +0500

    namespaces fix

--- src/backend/StudentProjects.DataLayer/Migrations/20251028180619_Initial.Designer.cs
+++ src/backend/StudentProjects.DataLayer/Migrations/20251028180619_Initial.Designer.cs
@@ -5,8 +5,6 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
-using StudentProjects.DataLayer;
-using StudentProjects.DataLayer;
 
 #nullable disable
 
