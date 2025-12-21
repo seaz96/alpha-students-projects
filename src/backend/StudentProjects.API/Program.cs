1013755bed37925f683f360b6f373fdc347da273
seaz96 <seaz96@proton.me>
2025-12-21 12:21:36 +0500

    some change

--- src/backend/StudentProjects.API/Program.cs
+++ src/backend/StudentProjects.API/Program.cs
@@ -39,7 +39,7 @@ builder.Services.AddCors(options =>
         corsPolicyBuilder =>
         {
             corsPolicyBuilder
-                .WithOrigins(builder.Configuration.GetValue<string>("CORS_ORIGINS")!)
+                .WithOrigins(builder.Configuration.GetValue<string>("CORS_ORIGINS")!.Split(','))
                 .AllowAnyMethod()
                 .AllowAnyHeader()
                 .SetIsOriginAllowedToAllowWildcardSubdomains()
